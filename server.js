e7d9dcd816b511f4662fa802ff7d689f40bc9ae6
Zoltan Toth <contact@zoltantoth.com>
2018-08-18 07:01:59 -0400

    Created mongoose schemas

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
 
 require('dotenv').config({ path: 'variables.env' });
 
+// Connects to database
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('DB connected'))
   .catch(err => console.error(err));
 
+// Initializes application
 const app = express();
 
 const PORT = process.env.port || 4444;
