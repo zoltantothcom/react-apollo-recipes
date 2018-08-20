477a437b1b56e9b0cea390ace268c74020aef2c7
Zoltan Toth <contact@zoltantoth.com>
2018-08-20 05:36:12 -0400

    Setup AppolloClient and ApolloProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:4444/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
