1cf91a9d9259891af141494ebdc185797a18f4c0
Zoltan Toth <contact@zoltantoth.com>
2018-08-21 05:21:36 -0400

    Hash user password with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -24,6 +25,23 @@ const UserSchema = new Schema({
     type: [Schema.Types.ObjectId],
     ref: 'Recipe',
   },
+});
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next;
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
