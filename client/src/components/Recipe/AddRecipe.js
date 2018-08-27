d34386d526c30f6a56408941be98585123ecaf9e
Zoltan Toth <contact@zoltantoth.com>
2018-08-27 06:29:55 -0400

    Scaffold ADD RECIPE form

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
 const AddRecipe = () => {
-  return <div>ADD RECIPE</div>;
+  return (
+    <div className="App">
+      <h2 className="App">Add Recipe</h2>
+      <form className="form">
+        <input
+          type="text"
+          name="name"
+          placeholder="Recipe Name"
+          onChange={this.handleChange}
+        />
+        <select name="category" onChange={this.handleChange}>
+          <option value="Breakfast">Breakfast</option>
+          <option value="Lunch">Lunch</option>
+          <option value="Dinner">Dinner</option>
+          <option value="Snack">Snack</option>
+        </select>
+        <input
+          type="text"
+          name="description"
+          placeholder="Add Description"
+          onChange={this.handleChange}
+        />
+        <textarea
+          name="instructions"
+          cols="30"
+          placeholder="Add Instructions"
+          onChange={this.handleChange}
+        />
+        <button type="submit" className="button-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddRecipe;
