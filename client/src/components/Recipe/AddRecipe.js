7078390f9be030dbd97c3d9a1cc0e68c1718b870
Zoltan Toth <contact@zoltantoth.com>
2018-08-28 05:46:11 -0400

    Make AddRecipe a stateful component

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,40 +1,62 @@
 import React from 'react';
 
-const AddRecipe = () => {
-  return (
-    <div className="App">
-      <h2 className="App">Add Recipe</h2>
-      <form className="form">
-        <input
-          type="text"
-          name="name"
-          placeholder="Recipe Name"
-          onChange={this.handleChange}
-        />
-        <select name="category" onChange={this.handleChange}>
-          <option value="Breakfast">Breakfast</option>
-          <option value="Lunch">Lunch</option>
-          <option value="Dinner">Dinner</option>
-          <option value="Snack">Snack</option>
-        </select>
-        <input
-          type="text"
-          name="description"
-          placeholder="Add Description"
-          onChange={this.handleChange}
-        />
-        <textarea
-          name="instructions"
-          cols="30"
-          placeholder="Add Instructions"
-          onChange={this.handleChange}
-        />
-        <button type="submit" className="button-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+class AddRecipe extends React.Component {
+  state = {
+    name: '',
+    instructions: '',
+    category: 'Breakfast',
+    description: '',
+    username: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { name, category, description, instructions } = this.state;
+
+    return (
+      <div className="App">
+        <h2 className="App">Add Recipe</h2>
+        <form className="form">
+          <input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Recipe Name"
+            onChange={this.handleChange}
+          />
+          <select name="category" value={category} onChange={this.handleChange}>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+            <option value="Snack">Snack</option>
+          </select>
+          <input
+            type="text"
+            name="description"
+            value={description}
+            placeholder="Add Description"
+            onChange={this.handleChange}
+          />
+          <textarea
+            name="instructions"
+            value={instructions}
+            cols="30"
+            placeholder="Add Instructions"
+            onChange={this.handleChange}
+          />
+          <button type="submit" className="button-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddRecipe;
