9b541be39a5544dbaf3de3cba7af4801ca272727
Zoltan Toth <contact@zoltantoth.com>
2018-09-02 19:25:32 -0400

    Add refetchQueries to addRecipe mutation

--- client/src/components/Profile/UserRecipes.js
+++ client/src/components/Profile/UserRecipes.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import { Link } from 'react-router-dom';
-import { GET_USER_RECIPES, DELETE_USER_RECIPE } from '../../queries';
+import {
+  GET_USER_RECIPES,
+  DELETE_USER_RECIPE,
+  GET_ALL_RECIPES,
+  GET_CURRENT_USER,
+} from '../../queries';
 
 const handleDelete = deleteUserRecipe => {
   const confirmDelete = window.confirm('Are you sure?');
@@ -34,6 +39,10 @@ const UserRecipes = ({ username }) => {
                 <Mutation
                   mutation={DELETE_USER_RECIPE}
                   variables={{ _id: recipe._id }}
+                  refetchQueries={() => [
+                    { query: GET_ALL_RECIPES },
+                    { query: GET_CURRENT_USER },
+                  ]}
                   update={(cache, { data: { deleteUserRecipe } }) => {
                     const { getUserRecipes } = cache.readQuery({
                       query: GET_USER_RECIPES,
