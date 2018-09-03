a76bb0e151d522609eb79fcde2eac86ddd231455
Zoltan Toth <contact@zoltantoth.com>
2018-09-03 11:40:45 -0400

    Client-side logic to properly toggle Like

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -5,32 +5,56 @@ import { LIKE_RECIPE } from '../../queries';
 
 class LikeRecipe extends Component {
   state = {
+    liked: false,
     username: '',
   };
 
   componentDidMount() {
     if (this.props.session.getCurrentUser) {
-      const { username } = this.props.session.getCurrentUser;
+      const { username, favorites } = this.props.session.getCurrentUser;
+      const { _id } = this.props;
+      const prevLiked =
+        favorites.findIndex(favorite => favorite._id === _id) > -1;
 
-      this.setState({ username });
+      this.setState({
+        username,
+        liked: prevLiked,
+      });
     }
   }
 
+  handleClick = likeRecipe => {
+    this.setState(
+      prevState => ({
+        liked: !prevState.liked,
+      }),
+      () => this.handleLike(likeRecipe),
+    );
+  };
+
   handleLike = likeRecipe => {
-    likeRecipe().then(({ data }) => {
-      console.log(data);
-    });
+    if (this.state.liked) {
+      likeRecipe().then(async ({ data }) => {
+        console.log(data);
+
+        await this.props.refetch();
+      });
+    } else {
+      console.log('unlike');
+    }
   };
 
   render() {
-    const { username } = this.state;
+    const { username, liked } = this.state;
     const { _id } = this.props;
 
     return (
       <Mutation mutation={LIKE_RECIPE} variables={{ _id, username }}>
         {likeRecipe =>
           username && (
-            <button onClick={() => this.handleLike(likeRecipe)}>Like</button>
+            <button onClick={() => this.handleClick(likeRecipe)}>
+              {liked ? 'Liked' : 'Like'}
+            </button>
           )
         }
       </Mutation>
