cf9dd41cb7f1c61c9b6f88e1f26caeeed2b47888
Zoltan Toth <contact@zoltantoth.com>
2018-09-04 06:13:20 -0400

    Display recipe image on home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,13 +8,13 @@ import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>Home</h1>
+    <h1 className="main-title">Find Recipes You Love</h1>
     <Query query={GET_ALL_RECIPES}>
       {({ data, loading, error }) => {
         if (loading) return <div>Loading...</div>;
         if (error) return <div>Error</div>;
         return (
-          <ul>
+          <ul className="cards">
             {data.getAllRecipes.map(recipe => (
               <RecipeItem key={recipe._id} {...recipe} />
             ))}
